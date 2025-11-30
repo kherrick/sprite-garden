6ecf332c06409e10a60dae2e26cf619397434ead
Karl Herrick <me@karlherrick.com>
2025-11-29 18:22:31 -0600

    Updates

---
+++ src/api/index.mjs
@@ -0,0 +1,18 @@
+import { characters } from "./characters.mjs";
+import { codeMap, keyMap } from "./keys.mjs";
+
+import { getDistanceFromPlayer } from "./player/getDistanceFromPlayer.mjs";
+import { getPlayerPosition } from "./player/getPlayerPosition.mjs";
+import { isPlayerNear } from "./player/isPlayerNear.mjs";
+
+import { SpriteGarden } from "./SpriteGarden.mjs";
+
+export {
+  characters,
+  codeMap,
+  keyMap,
+  getDistanceFromPlayer,
+  getPlayerPosition,
+  isPlayerNear,
+  SpriteGarden,
+};
